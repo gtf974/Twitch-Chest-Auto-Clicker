6f0e877982e58216506ee254b7f3039eb9d5ee80
Idotcom <55036586+gtf974@users.noreply.github.com>
2022-02-28 16:43:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (function autoClick(){
  	let array = Array.from(document.querySelectorAll('button')).filter(element => element.className.match(/\bScCoreButtonSuccess/));
 	if(array.length != 0) array.forEach(element => element.click());
+	console.log("[Twitch Chest Auto Clicker] Loot collected!");
  	setTimeout(()=>{requestAnimationFrame( autoClick )}, 1000);
 })()
