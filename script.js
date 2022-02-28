3dc476202b8771a4492ea5228e14abc42ee6fa9d
Idotcom <55036586+gtf974@users.noreply.github.com>
2022-02-28 16:45:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (function autoClick(){
- 	let array = Array.from(document.querySelectorAll('button')).filter(element => element.className.match(/\bScCoreButtonSuccess/));
-	if(array.length != 0) array.forEach(element => element.click());
-	console.log("[Twitch Chest Auto Clicker] Loot collected!");
- 	setTimeout(()=>{requestAnimationFrame( autoClick )}, 1000);
+    let array = Array.from(document.querySelectorAll('button')).filter(element => element.className.match(/\bScCoreButtonSuccess/));
+    if(array.length != 0){
+        array.forEach(element => element.click());
+        console.log("[Twitch Chest Auto Clicker] Loot collected!");
+    }
+    setTimeout(()=>{requestAnimationFrame( autoClick )}, 1000);
 })()
