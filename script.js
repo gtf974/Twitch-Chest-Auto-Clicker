57ebf660e2a6b6de55d9a62100aef608da08a856
Idotcom <55036586+gtf974@users.noreply.github.com>
2022-02-28 16:33:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function animate(){
-	requestAnimationFrame( animate );
+(function autoClick(){
   let array = Array.from(document.querySelectorAll('button')).filter(element => element.className.match(/\bScCoreButtonSuccess/));
+  console.log("lol")
 	if(array.length != 0) setTimeout(()=>{array.forEach(element => element.click());}, 1000);
-}
-animate();
+  setTimeout(()=>{requestAnimationFrame( autoClick )}, 1000);
+})()
