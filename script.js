d81d62fc8bcd31eef8d1d2963dfef2e35944862a
Idotcom <55036586+gtf974@users.noreply.github.com>
2022-02-28 16:37:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 (function autoClick(){
-  let array = Array.from(document.querySelectorAll('button')).filter(element => element.className.match(/\bScCoreButtonSuccess/));
-  console.log("lol")
+ 	let array = Array.from(document.querySelectorAll('button')).filter(element => element.className.match(/\bScCoreButtonSuccess/));
 	if(array.length != 0) array.forEach(element => element.click());
-  setTimeout(()=>{requestAnimationFrame( autoClick )}, 1000);
+ 	setTimeout(()=>{requestAnimationFrame( autoClick )}, 1000);
 })()
